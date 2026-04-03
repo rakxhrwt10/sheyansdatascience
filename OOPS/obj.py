8d18cee852879296ce91c7c4fb3a0b5f8e3a2a54
rakeshrawat12 <rakeshrawatr351@gmail.com>
2026-04-03 21:31:34 +0530

    object

---
+++ OOPS/obj.py
@@ -0,0 +1,20 @@
+class CarFactory:
+
+    a="BMW"
+    print(a)
+
+    def messaage(a):
+
+        print(f"hello{a}")
+
+
+bmw=CarFactory()
+
+CarFactory.messaage("Bmw")
+
+bmw.messaage("hello")
+
+
+
+
+
