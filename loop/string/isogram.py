24e69d3bcde931776eedc46c3f960cf6c8c022f3
rakeshrawat12 <rakeshrawatr351@gmail.com>
2026-04-11 10:20:42 +0530

    reverse

--- loop/string/isogram.py
+++ loop/string/isogram.py
@@ -11,5 +11,27 @@ def isogram(st: str):
     return True
 
 
-print(isogram("lamp"))   # True
+print(isogram("lampm"))   # True
 print(isogram("hello"))  # False
+
+
+# def isogram(st):
+#     st = st.lower()
+#     return len(st) == len(set(st))
+
+#brute force approach
+
+
+# def isogram(st: str):#hello
+#     st = st.lower()
+
+#     for i in range(len(st)):#len(st)-->5
+#         for j in range(i + 1, len(st)):#next element j-->e
+#             if st[i] == st[j]:#st[0]-->h st[1]-->e
+#                 return False
+
+#     return True
+
+
+# print(isogram("lamp"))   # True
+# print(isogram("hello"))  # False
---
+++ loop/string/revese.py
@@ -0,0 +1,9 @@
+def reverse(s:str):
+
+
+    return s[::-1]
+
+
+
+
+print(reverse("hello"))
