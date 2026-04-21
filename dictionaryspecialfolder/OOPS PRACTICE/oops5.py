ede21dbb14a14e76dd1bdf1826966b3eabfb129d
rakeshrawat12 <rakeshrawatr351@gmail.com>
2026-04-21 21:21:32 +0530

    oops5

---
+++ dictionaryspecialfolder/OOPS PRACTICE/oops5.py
@@ -0,0 +1,19 @@
+class BankAccount:
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print("Balance after deposit:", self.balance)
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            print("Balance after withdrawal:", self.balance)
+        else:
+            print("Insufficient balance")
+
+acc = BankAccount("Amit", 1000)
+acc.deposit(500)
+acc.withdraw(300)
